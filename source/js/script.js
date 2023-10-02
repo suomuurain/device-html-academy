42211f41b0a5ac1b72fb2f30d43ceec1d7c4d105
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-10-03 00:40:49 +0300

    добавляет скрипт для карты

--- source/js/script.js
+++ source/js/script.js
@@ -81,3 +81,19 @@ form.addEventListener('submit', function (evt) {
     form.reset();
   }
 });
+
+// Modal map
+
+const modalMap = document.querySelector('.map-modal');
+const linkMap = document.querySelector('.navigation__services-link--contacts');
+const closeMapBtn = modalMap.querySelector('.map-modal__close-button');
+
+linkMap.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalMap.classList.add('map-modal--show');
+});
+
+closeMapBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalMap.classList.remove('map-modal--show');
+});
